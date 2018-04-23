cc255e7d37a9270cfd52bb6cad60a302abf2b3e7
pattycakelol <pleung1415@gmail.com>
2018-04-22 22:17:16 -0700

    minor change, file deletion still works :)

--- uploadtest/delete.php
+++ uploadtest/delete.php
@@ -17,6 +17,8 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         // print_r($_POST['data']); //for testing purposes
         // $_POST['data'][$row['file_name']] <- this took me soooo long to get right
+        // the issue was that PHP converts the name field's special characters (such as periods and spaces) to underscores
+        // the work-around used was turning the name into an array so that the periods and spaces in file name in $_POST are left untouched
         $statement = "delete from logfiles where file_path = '".$_POST['data'][$row['file_name']]."'";
         if ($conn->query($statement) === TRUE) {
             echo "File deleted successfully<br>";
